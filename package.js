06a73ac02825aa534b1d522ce622cf6bddb00821
Isaac Snow <isaacjsnow@gmail.com>
2015-10-11 16:21:06 -0500

    Update package files

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'ijsnow:enum-manager',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'A simple package to keep track of enums',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/ijsnow/enum-manager.git',
   documentation: 'README.md'
 });
 
@@ -15,7 +11,7 @@ Package.onUse(function(api) {
   api.use('ecmascript');
 
   // Dependency
-  api.use('ijsnow:enum-object');
+  api.use('ijsnow:enum-object@0.0.1');
 
   api.addFiles('enum-manager.js');
 
@@ -25,6 +21,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('enum-manager');
+  api.use('ijsnow:enum-manager');
   api.addFiles('enum-manager-tests.js');
 });
