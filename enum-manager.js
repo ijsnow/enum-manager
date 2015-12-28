6ce27b965da6f5f0af0894e536ec2415d0936b02
Isaac Snow <isaacjsnow@gmail.com>
2015-12-28 16:30:45 -0600

    Refactor to ES6 class

--- enum-manager.js
+++ enum-manager.js
@@ -1,54 +1,57 @@
 /*
- * @summary: An enum manager object
- * @param: name
- *    @type: Object (Array)
- *    @description: array of objects containing a name and values or an array of EnumObjects
- *    @optional: true
- * @returns: an EnumManager
+ * An object to help manage your enums in your project.
+ * Uses EnumObjects (ijsnow:enum-objects)
  */
-EnumManager = function (enums) {
-  this.props = {};
-  // If enums were passed in in an array, add init the manager with the enums.
-  if (typeof enums === "object") {
-    this.props._enums = _init(enums);
-  } else {
-    this.props._enums = [];
+EnumManager = class {
+  /*
+   * @summary: An enum manager object
+   * @param: name
+   *    @type: Object (Array)
+   *    @description: array of objects containing a name and values or an array of EnumObjects
+   *    @optional: true
+   * @returns: an EnumManager
+   */
+  constructor (enums) {
+    this.props = {};
+    // If enums were passed in in an array, add init the manager with the enums.
+    if (typeof enums === "object") {
+      this.props._enums = _init(enums);
+    } else {
+      this.props._enums = [];
+    }
   }
 
-  return this;
-}
-
-// @summary: Adds an enum
-// @param: vals
-//    @type: Object (Array)
-//    @optional: false
-EnumManager.prototype.add = function (en) {
-  this.props._enums.push(_createEnum(en));
-};
-
-// @summary: Returns the mapped key or value by calling the EnumObject's map method.
-// @param: name
-//    @type: String
-//    @description: Name of the EnumObject you want to map.
-//    @optional: false
-// @param: input
-//    @type: String || Number
-//    @description: Is either the key or value you want to map
-//    @optional: false
-// @returns: If passed a string, returns a number that represents the key,
-//           otherwise, return the string representing the value.
-EnumManager.prototype.map = function (name, input) {
-  return _getEnum.call(this, name).map(input);
-};
-
-// Get all or just one enum
-EnumManager.prototype.get = function (name) {
-  debugger;
-  if (name) {
-    return _.findWhere(this.props._enums, { name: name });
+  // @summary: Adds an enum
+  // @param: vals
+  //    @type: Object (Array)
+  //    @optional: false
+  add (en) {
+    _addEnum.call(this, en);
   }
 
-  return this.props._enums;
+  // @summary: Returns the mapped key or value by calling the EnumObject's map method.
+  // @param: name
+  //    @type: String
+  //    @description: Name of the EnumObject you want to map.
+  //    @optional: false
+  // @param: input
+  //    @type: String || Number
+  //    @description: Is either the key or value you want to map
+  //    @optional: false
+  // @returns: If passed a string, returns a number that represents the key,
+  //           otherwise, return the string representing the value.
+  map (name, input) {
+    return _getEnum.call(this, name).map(input);
+  }
+
+  // Get all or just one enum
+  get (name) {
+    if (name) {
+      return _.findWhere(this.props._enums, { name: name });
+    }
+
+    return this.props._enums;
+  }
 }
 
 // Private methods
@@ -59,6 +62,13 @@ function _init(enums) {
   };
 
   return enums.map(_mapper);
+}
+
+// Create an EnumObject and add it to EnumManager.props._values
+// Copies old enums and adds the new one.
+function _addEnum(en) {
+  this.props._enums = [].slice(this.props._enums)
+                        .concat(_createEnum(en));
 }
 
 // Gets the EnumObject with the name that is passed in
